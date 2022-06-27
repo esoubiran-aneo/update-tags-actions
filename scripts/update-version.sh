9858ba80fffe3e88893a235434c20b81f4df8fb0
Estéban Soubiran <esoubiran@aneo.fr>
2022-06-27 10:19:40 +0200

    feat: create scripts

---
+++ scripts/update-version.sh
@@ -0,0 +1,11 @@
+# Get value from GITHUB_REF_NAME
+# Update package.json and package-lock.json
+# Update CHANGELOG.md
+
+# Get value from GITHUB_REF_NAME (e.g. v1.0.0)
+VERSION=$(echo $GITHUB_REF_NAME | sed 's/^v//')
+
+# Update package.json and package-lock.json
+sed -i "s/\"version\": \".*\"/\"version\": \"$VERSION\"/g" package.json package-lock.json
+
+head package.json package-lock.json
