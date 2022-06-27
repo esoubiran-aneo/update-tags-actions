6cbda469eaede865e2515a05849f0253a6ebd4da
Estéban Soubiran <esoubiran@aneo.fr>
2022-06-27 10:42:57 +0200

    fix: update action

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,9 +1,5 @@
-# Get value from GITHUB_REF_NAME
-# Update package.json and package-lock.json
-# Update CHANGELOG.md
-
 # Get value from GITHUB_REF_NAME (e.g. v1.0.0)
 VERSION=$(echo $GITHUB_REF_NAME | sed 's/^v//')
 
-# Update package.json and package-lock.json
-sed -i "s/\"version\": \".*\"/\"version\": \"$VERSION\"/g" package.json package-lock.json
+# Update package.json
+sed -i "s/\"version\": \".*\"/\"version\": \"$VERSION\"/g" package.json
